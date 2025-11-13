c2dfea8b31864eb7de1bfac8273dd0edb3c3764b
Victor Calazans Ramos <victorcalazansramos@gmail.com>
2025-11-13 14:14:32 -0300

    feat: creating wrapper for apollo client and initialize connection

---
+++ src/lib/apollo-client.ts
@@ -0,0 +1,18 @@
+'use client';
+
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+
+const httpLink = new HttpLink({
+    uri: 'https://rickandmortyapi.com/graphql',
+});
+
+export const apolloClient = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache(),
+    defaultOptions: {
+        watchQuery: {
+            fetchPolicy: 'cache-and-network',
+        },
+    },
+});
+
